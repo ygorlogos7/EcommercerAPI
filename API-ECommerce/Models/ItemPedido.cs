f4f5f06fee80cc9ba70faf8bf17da9e2c488cdf1
Ygor Nonato <99219793+ygorlogos7@users.noreply.github.com>
2025-04-25 21:52:45 -0300

    atualizando

--- API-ECommerce/Models/ItemPedido.cs
+++ API-ECommerce/Models/ItemPedido.cs
@@ -13,12 +13,9 @@ public partial class ItemPedido
 
     public int Quantidade { get; set; }
 
-    public virtual Pedido Pedido { get; set; } = null!;
-
-    public virtual Produto Produto { get; set; } = null!;
-
     public virtual Pedido? IdPedidoNavigation { get; set; }
 
     public virtual Produto? IdProdutoNavigation { get; set; }
-
+    public object Pedido { get; internal set; }
+    public object Produto { get; internal set; }
 }
